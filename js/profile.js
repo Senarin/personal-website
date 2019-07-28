a8455512dbe79f1d1fdbb9a98d32b8447ec3f8a8
Hanaya <themunyang21@naver.com>
2019-07-28 23:13:01 +0900

    ...

---
+++ js/profile.js
@@ -0,0 +1,7 @@
+var listElem = "list_profiles";
+
+function toggleAccountList(){
+ toggleVisiblities("show_list_profiles",1);
+ toggleVisiblities("hide_list_profiles",1);
+ toggleVisiblities(listElem);
+}
