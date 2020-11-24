578d8f609f1163724e0102f69ec16dd19d4270cb
Yukina <themunyang21@naver.com>
2020-11-24 19:37:51 +0900

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -3,7 +3,7 @@ function toggleAccountList(){
  toggleVisiblities("hide_list_profiles","inline-block");
 
  toggleVisiblities("list_profiles","initial");
- toggleVisiblities("discord_widget","initial");
+ toggleVisiblities("discord_widget","inline-block");
 }
 
 
